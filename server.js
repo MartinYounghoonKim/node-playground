236be6c79cad46f910dea9ec03e09623eb6d01ae
martinyounghoonkim <hoons0131@gmail.com>
2017-11-06 23:46:40 +0900

    session을 통한 login 기능 추가

--- server.js
+++ server.js
@@ -1,75 +1,75 @@
 const express = require('express');
 const app = express();
+const session = require('express-session');
+const bodyParser = require('body-parser');
 const cookieParser = require('cookie-parser');
 
-
-/**
- * @desc 정적 파일 관리
- */
-app.use(express.static('public'));
-
-/**
- * @desc Cookie 사용
- */
-app.use(cookieParser('1235dgsdfgi15'));
-
-var products = {
-  1: { title:'The history of web 1' },
-  2: { title:'The history of web 2' },
-  3: { title:'The history of web 3' }
-};
-
-app.get('/products', function(req, res){
-  var output = '';
-  for(var product in products){
-    output += `<li><a href="/cart/${product}">${products[product].title}</a></li>`;
-  }
-  res.send(`<h1>Products</h1><ul>${output}</ul><a href="/cart">cart</a>`);
-});
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(session({
+  secret: 'keyboard cat',
+  resave: false,
+  saveUninitialized: true
+}));
 
 app.get('/count', function(req, res){
-  if(req.signedCookies.count){
-    var count = parseInt(req.signedCookies.count);
+  if(req.session.count){
+    req.session.count++;
   } else {
-    var count = 0;
+    req.session.count = 1;
   }
-  count = count + 1;
-  res.cookie('count', count, {signed: true});
-  res.send('count : ' + count);
+  res.send('result: ' + req.session.count);
 });
 
-app.get('/cart/:id', function(req,res){
-  var id = req.params.id;
-  if(req.signedCookies.cart){
-    var cart = req.signedCookies.cart;
-  } else {
-    var cart = {};
-  }
-
-  if(!cart[id]){ 
-    cart[id] = 0;
-  }
-
-  cart[id] = parseInt(cart[id])+1;
-  res.cookie('cart', cart, {signed: true});
-  res.redirect('/cart');
+app.get('/auth/login', function(req, res){
+  var output = `
+  <h1>Login</h1>
+  <form action="/auth/login" method="post">
+      <p>
+        <input type="text" name="username" placeholder="username">
+      </p>
+      <p>
+        <input type="password" name="password" placeholder="password">
+      </p>
+      <input type="submit"/>
+  </form>
+  `;
+  res.send(output);
 });
-
-app.get('/cart', function(req,res){
-  var cart= req.signedCookies.cart;
-  if(!cart) {
-    res.send('Empty!');
+app.get('/auth/logout', function(req,res){
+  delete req.session.displayName;
+  res.redirect('/welcome');
+})
+app.get('/welcome', function(req,res){
+  //로그인 성공시
+  if(req.session.displayName){
+    res.send(`
+      <h1>Hello, ${req.session.displayName}</h1>
+      <a href="/auth/logout">Logout</a>
+    `);
   } else {
-    var output = '';
-    for(var id in cart){
-      output += `<li>${products[id].title} (${cart[id]})</li>`;
-    }
+    res.send(`
+      <h1>Welcome</h1>
+      <a href="/auth/login">Login</a>
+    `)
   }
-  res.send(`
-    <h1>Cart</h1>
-    <ul>${output}</ul>
-    <a href="/products">Products List</a>
-  `);
+  
+})
+app.post('/auth/login', function(req, res){
+  var user = {
+    username: 'martin',
+    password: '111',
+    displayName: 'Martin'
+  };
+  var uname = req.body.username;
+  var pwd = req.body.password;
+
+  //유저 정보가 일치할 경우
+  if(uname === user.username && pwd === user.password){
+    req.session.displayName = user.displayName;
+    res.redirect('/welcome');
+  } else {
+    res.send('error<a href="/auth/login">login</a>');
+  }
 });
 
 app.listen(3000, function () {
